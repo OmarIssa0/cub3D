68b54d609c25d7bbc67d6845055e272d7c115985
OmarIssa0 <omar.issa.slwade@gmail.com>
2025-03-20 23:45:05 +0300

    initialize cub3D project with Makefile and basic structure

---
+++ Makefile
@@ -0,0 +1,35 @@
+NAME = cub3d
+CC = cc
+CFLAGS = -Wall -Wextra -Werror -Iinclude -I$(LIBFT_DIR)
+LIBS = -I/usr/local/include -L/usr/local/lib -lmlx42 -ldl -lglfw -pthread -lm 
+LIBFT_DIR = ./Libft
+LIBFT = $(LIBFT_DIR)/libft.a
+
+SRC = src/main.c
+
+OBJ_DIR = obj
+OBJS = $(addprefix $(OBJ_DIR)/, $(SRC:.c=.o))
+
+all : $(LIBFT) $(NAME)
+
+$(NAME) : $(OBJS)
+	$(CC) $(CFLAGS) $(OBJS) $(LIBFT) $(LIBS) -o $(NAME)
+
+$(OBJ_DIR)/%.o: %.c
+	@mkdir -p $(dir $@)
+	@$(CC) $(CFLAGS) -c $< -o $@
+
+$(LIBFT) :
+	make -C $(LIBFT_DIR)
+
+clean :
+	rm -rf $(OBJ_DIR)
+	make clean -C $(LIBFT_DIR)
+
+fclean : clean
+	rm -f $(NAME)
+	make fclean -C $(LIBFT_DIR)
+
+re : fclean all
+
+.PHONY : all clean fclean re
---
+++ include/cub3d.h
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   cub3d.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: oissa <oissa@student.42amman.com>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/03/20 23:35:38 by oissa             #+#    #+#             */
+/*   Updated: 2025/03/20 23:37:17 by oissa            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef CUB3D_H
+# define CUB3D_H
+
+# include <stdio.h>
+# include <stdlib.h>
+# include <unistd.h>
+# include <fcntl.h>
+# include <math.h>
+# include <string.h>
+# include <errno.h>
+# include <limits.h>
+# include <MLX42/MLX42.h>
+# include <time.h>
+# include "libft.h"
+
+#endif
